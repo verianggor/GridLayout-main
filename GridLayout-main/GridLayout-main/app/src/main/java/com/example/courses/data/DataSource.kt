fe97b77ef0a90993f017097be5abc4281bdbd6b8
verianggor <veryanggoro1000@gmail.com>
2024-12-11 22:34:04 +0700

    nim 234311055

---
+++ GridLayout-main/GridLayout-main/app/src/main/java/com/example/courses/data/DataSource.kt
@@ -0,0 +1,20 @@
+package com.example.courses.data
+
+import com.example.courses.R
+import com.example.courses.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.miya, 1, R.drawable.miya),
+        Topic(R.string.tigreal, 2, R.drawable.tigreal),
+        Topic(R.string.alucard, 3, R.drawable.alucard),
+        Topic(R.string.saber, 4, R.drawable.saber),
+        Topic(R.string.layla, 5, R.drawable.layla),
+        Topic(R.string.zilong, 6, R.drawable.zilong),
+        Topic(R.string.eudora, 7, R.drawable.eudora),
+        Topic(R.string.franco, 8, R.drawable.franco),
+        Topic(R.string.sun, 9, R.drawable.sun),
+        Topic(R.string.chou, 10, R.drawable.chou),
+
+        )
+}
